d7df61bf68783c1a494fc1b3d1a97a907b624a62
Tarcila Cardoso Alves <tarcilacardosoalves@gmail.com>
2023-05-16 20:25:34 -0300

    Mai uma classe abstrata

---
+++ java-polimorfismo/bytebank-herdado/src/Admisnistrador.java
@@ -0,0 +1,9 @@
+
+public class Admisnistrador extends FuncionarioAutenticavel {
+
+    @Override
+    public double getBonificacao() {
+        return 50;
+    }
+
+}
--- java-polimorfismo/bytebank-herdado/src/Gerente.java
+++ java-polimorfismo/bytebank-herdado/src/Gerente.java
@@ -1,22 +1,9 @@
-public class Gerente extends Funcionario {
-	
-	private int senha;
+//Gerente eh um FuncionarioAutenticavel, Gerente herda da classe FuncionarioAutenticavel
 
-	public double getBonificacao() {
-		System.out.println("Chamando o método de bonificacao do GERENTE");
-		return super.getSalario();
-	}
+public class Gerente extends FuncionarioAutenticavel {
 
-	public void setSenha(int senha) {
-		this.senha = senha;
-	}
-
-	public boolean autentica(int senha) {
-		if(this.senha == senha){
-			return true;
-		} else {
-			return false;
-		}
-	}
-
+        public double getBonificacao() {
+            System.out.println("Chamando o método de bonificacao do GERENTE");
+            return super.getSalario();
+        }
 }

2b6dee8df556337d0b6d1ec10f12b72c3c92cab4
Tarcila Cardoso Alves <tarcilacardosoalves@gmail.com>
2023-05-20 19:53:26 -0300

    Sacando com Unchecked Exception

--- java-pilhaDeExecucao/src/TestaContaComExcecaoCheckd.java
+++ java-pilhaDeExecucao/src/TestaContaComExcecaoCheckd.java
@@ -1,9 +1,13 @@
+import javax.management.MBeanException;
 
 public class TestaContaComExcecaoCheckd {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-
+    public static void main(String[] args) {
+        Conta c = new Conta();
+        try {
+            c.deposita();
+        } catch(MinhaExcecao ex) {
+            System.out.println("tratamento ......");
+        }
+    }
 }

dc318c811f2ad0c22fb6421e0b4dd6cb03969c76
Tarcila Cardoso Alves <tarcilacardosoalves@gmail.com>
2023-05-20 20:10:34 -0300

    Aplicando exceções

--- java-polimorfismo/bytebank-herdado-conta/src/TesteSaca.java
+++ java-polimorfismo/bytebank-herdado-conta/src/TesteSaca.java
@@ -2,13 +2,15 @@
 public class TesteSaca {
 
 	public static void main(String[] args) {
-		Conta conta = new ContaCorrente(123, 321);
+		 Conta conta = new ContaCorrente(123, 321);
 
-        conta.deposita(200.0);
-        conta.saca(190.0);
-
-        System.out.println(conta.getSaldo());
-
-	}
+		    conta.deposita(200.0);
+		    try {
+		        conta.saca(210.0);
+		    } catch(SaldoInsuficienteException ex) {
+		        System.out.println("Ex: " + ex.getMessage());
+		    }
+		    System.out.println(conta.getSaldo());
+		}
 
 }

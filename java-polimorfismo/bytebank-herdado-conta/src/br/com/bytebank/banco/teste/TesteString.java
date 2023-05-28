60861c93344b88899d9eb0fbcd43cc39fac09afc
Tarcila Cardoso Alves <tarcilacardosoalves@gmail.com>
2023-05-27 22:20:59 -0300

    Metodos da classe string

--- java-polimorfismo/bytebank-herdado-conta/src/br/com/bytebank/banco/teste/TesteString.java
+++ java-polimorfismo/bytebank-herdado-conta/src/br/com/bytebank/banco/teste/TesteString.java
@@ -3,16 +3,40 @@ package br.com.bytebank.banco.teste;
 public class TesteString {
 
 	public static void main(String[] args) {
+		
+		String vazio ="    Alura    ";
+		String outroVazio = vazio.trim(); //Tira os espaços
+
+		System.out.println(outroVazio.contains("Alu"));
 
 
         String nome = "Alura"; //object literal
         
-        //String outra = nome.replace("A", "a");
-        
-        String outra = nome.toLowerCase();
+        System.out.println(nome.length());
 
-        System.out.println(nome);
-        System.out.println(outra);
+        for(int i = 0; i < nome.length(); i++) {
+            System.out.println(nome.charAt(i));
+        }
+        
+        	
+        
+//        	String sub = nome.substring(1);
+//        	System.out.println(sub);
+        
+//          int pos = nome.indexOf("ur");
+//          System.out.println(pos);
+//        
+//        char c = nome.charAt(2);
+//        System.out.println(c);
+        
+//        char c = 'A';
+//        char d = 'a';
+//        String outra = nome.replace(c,d);
+        
+        //String outra = nome.toLowerCase();
+
+//        System.out.println(nome);
+//        System.out.println(outra);
 	}
 
 }

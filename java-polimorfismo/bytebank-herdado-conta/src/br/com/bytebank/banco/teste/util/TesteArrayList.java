1e8a717bc6572c7844e9a4e45e2746f3e5da3f67
Tarcila Cardoso Alves <tarcilacardosoalves@gmail.com>
2023-06-02 23:55:18 -0300

    list e Linkedlist

--- java-polimorfismo/bytebank-herdado-conta/src/br/com/bytebank/banco/teste/util/TesteArrayList.java
+++ java-polimorfismo/bytebank-herdado-conta/src/br/com/bytebank/banco/teste/util/TesteArrayList.java
@@ -1,6 +1,7 @@
 package br.com.bytebank.banco.teste.util;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 
 import br.com.bytebank.banco.modelo.Conta;
 import br.com.bytebank.banco.modelo.ContaCorrente;
@@ -8,7 +9,7 @@ import br.com.bytebank.banco.modelo.ContaCorrente;
 public class TesteArrayList {
 
 	public static void main(String[] args) {
-	     ArrayList<Conta> lista = new ArrayList<Conta>();
+	     LinkedList<Conta> lista = new LinkedList<Conta>();
 
          Conta cc = new ContaCorrente(22, 11);
          lista.add(cc);
